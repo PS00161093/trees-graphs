529c757ff1888a6d7427a893acc6d834709b2d48
ps29653 <SharmaPrashant@JohnDeere.com>
2020-12-08 22:27:41 +0530

    added insert() method

--- src/App.java
+++ src/App.java
@@ -4,7 +4,18 @@ class App {
 
     public static void main(String[] args) {
 
+        MyTree tree = new MyTree();
+        tree.insert(9);
+        tree.insert(4);
+        tree.insert(6);
+        tree.insert(20);
+        tree.insert(170);
+        tree.insert(15);
+        tree.insert(1);
 
-
+        MyTree.MyTreeNode data = tree.traverse(tree.rootNode);
+        System.out.println(data.leftChild);
+        System.out.println(data.rightChild);
+        System.out.println(data.value);
     }
 }
--- src/MyTree.java
+++ src/MyTree.java
@@ -2,7 +2,44 @@ package src;
 
 public class MyTree {
 
+    public MyTreeNode rootNode;
 
+    public void insert(int newValue) {
+
+        MyTreeNode newNode = new MyTreeNode(newValue);
+
+        if (this.rootNode == null) {
+
+            this.rootNode = newNode;
+
+        } else {
+
+            MyTreeNode currentNode = this.rootNode;
+            while (true) {
+                if (newValue < currentNode.value) {
+                    if (currentNode.leftChild == null) {
+                        currentNode.leftChild = newNode;
+                        break;
+                    }
+                    currentNode = currentNode.leftChild;
+                } else {
+                    if (currentNode.rightChild == null) {
+                        currentNode.rightChild = newNode;
+                        break;
+                    }
+                    currentNode = currentNode.rightChild;
+                }
+            }
+        }
+    }
+
+    public MyTreeNode traverse(MyTreeNode node) {
+
+        MyTreeNode tree = node;
+        tree.leftChild = node.leftChild == null ? null : traverse(node.leftChild);
+        tree.rightChild = node.rightChild == null ? null : traverse(node.rightChild);
+        return tree;
+    }
 
     static class MyTreeNode {
 
