04653f160d2725685170b4bcbc51dded99caacb7
ps29653 <SharmaPrashant@JohnDeere.com>
2020-12-08 08:33:12 +0530

    Custom Implementation of Tree

---
+++ src/MyTree.java
@@ -0,0 +1,23 @@
+package src;
+
+public class MyTree {
+
+
+
+    static class MyTreeNode {
+
+        int value;
+
+        MyTreeNode leftChild;
+
+        MyTreeNode rightChild;
+
+        MyTreeNode(int value) {
+
+            this.value = value;
+            this.leftChild = null;
+            this.rightChild = null;
+        }
+    }
+
+}
