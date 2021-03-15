a9dcc4247b06a14c4dac4a2d0e846621aaa3a078
ps29653 <SharmaPrashant@JohnDeere.com>
2021-03-15 22:21:57 +0530

    symmetric-tree

--- trees/src/SymmetricTree.java
+++ trees/src/SymmetricTree.java
@@ -9,7 +9,7 @@ public class SymmetricTree {
 
     public boolean isSymmetric(TreeNode root) {
 
-        return isSym(root, root);
+        return isSymmetric(root, root);
     }
 
     /**
@@ -17,11 +17,11 @@ public class SymmetricTree {
      * @param t2
      * @return true if symmetric else, false.
      */
-    public boolean isSym(TreeNode t1, TreeNode t2) {
+    public boolean isSymmetric(TreeNode t1, TreeNode t2) {
 
         if (t1 == null && t2 == null) return true;
         if (t1 == null || t2 == null) return false;
 
-        return t1.val == t2.val && isSym(t1.left, t2.right) && isSym(t1.right, t2.left);
+        return t1.val == t2.val && isSymmetric(t1.left, t2.right) && isSymmetric(t1.right, t2.left);
     }
 }
