f916ff4d52b18d61c0025a5c97384854eebffe6e
ps29653 <SharmaPrashant@JohnDeere.com>
2021-03-15 22:22:34 +0530

    symmetric-tree

--- trees/src/SymmetricTree.java
+++ trees/src/SymmetricTree.java
@@ -22,6 +22,8 @@ public class SymmetricTree {
         if (t1 == null && t2 == null) return true;
         if (t1 == null || t2 == null) return false;
 
-        return t1.val == t2.val && isSymmetric(t1.left, t2.right) && isSymmetric(t1.right, t2.left);
+        return t1.val == t2.val
+                && isSymmetric(t1.left, t2.right)
+                && isSymmetric(t1.right, t2.left);
     }
 }
