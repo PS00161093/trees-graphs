8714febfefca163460a07bc33d6fd5ce9721ff74
ps29653 <SharmaPrashant@JohnDeere.com>
2021-04-17 20:15:21 +0530

    clone-a-binary-tree

--- trees/src/RightView.java
+++ trees/src/RightView.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class LeftViewTree {
+public class RightView {
 
-    public ArrayList<Integer> leftView(Node root) {
+    public ArrayList<Integer> rightView(Node root) {
 
         ArrayList<Integer> result = new ArrayList<>();
         if (root == null) return result;
@@ -17,7 +17,7 @@ public class LeftViewTree {
             int n = queue.size();
             for (int i = 1; i <= n; i++) {
                 Node temp = queue.poll();
-                if (i == 1) result.add(temp.data);
+                if (i == n) result.add(temp.data);
                 if (temp.left != null) queue.add(temp.left);
                 if (temp.right != null) queue.add(temp.right);
             }
@@ -25,5 +25,16 @@ public class LeftViewTree {
 
         return result;
     }
+
 }
 
+class Node {
+
+    int data;
+    Node left, right;
+
+    public Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
