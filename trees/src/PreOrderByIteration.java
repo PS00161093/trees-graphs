ab56547e332dac40970413d7d6061aa654bd57b7
ps29653 <SharmaPrashant@JohnDeere.com>
2021-04-18 12:30:25 +0530

    pre-order-by-iteration

---
+++ trees/src/PreOrderByIteration.java
@@ -0,0 +1,26 @@
+package trees.src;
+
+import java.util.Stack;
+
+/**
+ * URL: https://www.baeldung.com/java-depth-first-search
+ */
+public class PreOrderByIteration {
+
+    public void preOrder(TreeNode root) {
+
+        if (root != null) {
+
+            Stack<TreeNode> stack = new Stack<>();
+            stack.push(root);
+
+            while (!stack.isEmpty()) {
+                TreeNode currentNode = stack.pop();
+                System.out.println(currentNode.val);
+
+                if (currentNode.left != null) stack.push(currentNode.left);
+                if (currentNode.right != null) stack.push(currentNode.right);
+            }
+        }
+    }
+}
