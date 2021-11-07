65c11f0376e0faf11893371992616251b5d2b793
Prashant Sharma <prashant.sharma1thoughtworks.com>
2021-11-07 14:32:58 +0530

    merge-two-binary-trees | O(n)

--- trees/src/easy/MergeBinaryTree.java
+++ trees/src/easy/MergeBinaryTree.java
@@ -1,22 +1,18 @@
-package trees.src;
+package trees.src.easy;
+
+import trees.src.TreeNode;
 
 /**
- * @author Prashant Sharma
+ * https://leetcode.com/problems/merge-two-binary-trees/
  */
 public class MergeBinaryTree {
 
-    /**
-     * @param tree1: first binary tree
-     * @param tree2: second binary tree
-     * @return merged binary tree
-     */
     public TreeNode mergeTress(TreeNode tree1, TreeNode tree2) {
 
         if (tree1 == null) return tree2;
         if (tree2 == null) return tree1;
 
         tree1.val += tree2.val;
-
         tree1.left = mergeTress(tree1.left, tree2.left);
         tree1.right = mergeTress(tree1.right, tree2.right);
 
