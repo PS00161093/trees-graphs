1d8a54682e00aa6602e54b9f18469962bf809edf
Prashant Sharma <prashant.sharma1thoughtworks.com>
2021-11-22 22:34:33 +0530

    preOrder transversal by iteration

--- trees/src/PreOrderByIteration.java
+++ trees/src/PreOrderByIteration.java
@@ -8,18 +8,16 @@ import java.util.Stack;
 public class PreOrderByIteration {
 
     public void preOrder(TreeNode root) {
-
         if (root != null) {
-
             Stack<TreeNode> stack = new Stack<>();
             stack.push(root);
-
             while (!stack.isEmpty()) {
                 TreeNode currentNode = stack.pop();
                 System.out.println(currentNode.val);
-
-                if (currentNode.left != null) stack.push(currentNode.left);
-                if (currentNode.right != null) stack.push(currentNode.right);
+                if (currentNode.left != null)
+                    stack.push(currentNode.left);
+                if (currentNode.right != null)
+                    stack.push(currentNode.right);
             }
         }
     }
