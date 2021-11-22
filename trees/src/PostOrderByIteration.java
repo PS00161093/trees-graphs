e1f190dcc56f89652aa4b6942c00abfdf8b884f3
Prashant Sharma <prashant.sharma1thoughtworks.com>
2021-11-22 22:37:06 +0530

    postOrder transversal by iteration

--- trees/src/PostOrderByIteration.java
+++ trees/src/PostOrderByIteration.java
@@ -12,27 +12,24 @@ import java.util.Stack;
 public class PostOrderByIteration {
 
     public void postOrder(TreeNode root) {
-
         if (root != null) {
-
             Stack<TreeNode> stack = new Stack<>();
             TreeNode previous = root;
             TreeNode current;
             stack.push(root);
-
             while (!stack.isEmpty()) {
-
                 current = stack.peek();
                 boolean hasChild = current.left != null || current.right != null;
                 boolean isPrevLastChild = previous == current.right || previous == current.left && current.right == null;
-
                 if (!hasChild || isPrevLastChild) {
                     current = stack.pop();
                     System.out.println(current.val);
                     previous = current;
                 } else {
-                    if (current.right != null) stack.push(current.right);
-                    if (current.left != null) stack.push(current.left);
+                    if (current.right != null)
+                        stack.push(current.right);
+                    if (current.left != null)
+                        stack.push(current.left);
                 }
             }
         }
